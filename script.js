c2b8d8f0f59bc317646f9d17826bc01106cedb7f
Tamas Farago <tamas@Tamass-MacBook-Pro.local>
2020-09-09 01:46:06 +0200

    added javascript for menu page

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("kaki")
+
+
+let main = document.querySelector("#main-c")
+let drink = document.querySelector(".drinks")
+
+function mainfunc(){
+    main.classList.remove("remove");
+    drink.classList.add("remove")
+}
+
+function drinkfunc(){
+    drink.classList.remove("remove");
+    main.classList.add("remove")
+}
+
+console.log("kaki")
