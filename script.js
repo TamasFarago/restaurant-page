371021e32185e9ba528872bb98e55174c3be9d78
Tamas Farago <tamas@Tamass-MacBook-Pro.local>
2020-09-09 14:42:51 +0200

    finish menu

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-console.log("kaki")
-
-
 let main = document.querySelector("#main-c")
 let drink = document.querySelector(".drinks")
+let dessert = document.querySelector(".dessert")
 
 function mainfunc(){
     main.classList.remove("remove");
     drink.classList.add("remove")
+    dessert.classList.add("remove")
 }
 
 function drinkfunc(){
     drink.classList.remove("remove");
-    main.classList.add("remove")
+    main.classList.add("remove");
+    dessert.classList.add("remove");
 }
 
-console.log("kaki")
+function dessertfunc(){
+    dessert.classList.remove("remove");
+    main.classList.add("remove");
+    drink.classList.add("remove");
+}
+
+
